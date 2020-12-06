aff4ff5bf4813e04b5d90ef3898e65c0faa8d2a4
olligobber <sirolligobbervii@yahoo.com.au>
2020-12-06 16:48:12 +1100

    Minified further

--- Day6Task2minify.hs
+++ Day6Task2minify.hs
@@ -1,3 +1,3 @@
 import Data.List.Split (splitOn)
-import Data.Set as S
-main = interact $ show . sum . fmap (length . foldl1 S.intersection . fmap S.fromList . lines) . splitOn "\n\n"
+import Data.List (intersect)
+main = interact $ show . sum . fmap (length . foldl1 intersect . lines) . splitOn "\n\n"
